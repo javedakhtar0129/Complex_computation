d67695b959dc7ceaccdf7f1523c82cca1f3e3953
JAVED AKHTAR <javedakhtar@JAVEDs-MacBook-Air.local>
2022-01-20 13:39:02 +0530

    20 Jan

---
+++ abAlgorithm.py
@@ -0,0 +1,13 @@
+def algorithm(real,imaginary,n,plots):
+    if n%2 == 0:
+        real = till_n(real,imaginary,n)
+        print(real)
+
+
+def till_n(real,imaginary,n):
+    k = 1
+    result = 0
+    while(k <= n):
+        print("")
+        result +=  
+
