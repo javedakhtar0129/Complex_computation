a9d03b8a6bf3c44355e15eb1d5788a369e71c4d4
JAVED AKHTAR <javedakhtar@JAVEDs-MacBook-Air.local>
2022-01-21 12:40:14 +0530

    new odd/even func

--- abAlgorithm.py
+++ abAlgorithm.py
@@ -1,13 +1,81 @@
-def algorithm(real,imaginary,n,plots):
-    if n%2 == 0:
-        real = till_n(real,imaginary,n)
-        print(real)
+# using recursion for combination
+def combination(n,r):
+    if r==0:
+        return 1
+    elif n<r:
+        return 0
+    else:
+        return combination(n-1,r-1) + combination(n-1,r)
+
+# gives sign of the iota, lambda expression
+def iota(i):
+    return [1, 1, -1, -1][i % 4]
+
+# def till_n(real,imaginary,n):
+#     k = 1
+#     result = 0
+#     while(k <= n):
+#         result += combination(n,k)*(real**k)*(imaginary**(n-k))*iota(n-k)
+#         k+=2
+#
+#     return result
 
 
-def till_n(real,imaginary,n):
+def even_real(real,imaginary,n):
+    k = 0
+    result = 0
+    while(k <= n ):
+        result += combination(n, k) * (real ** k) * (imaginary ** (n - k)) * iota(n - k)
+        k += 2
+
+    return result
+
+
+def even_imaginary(real,imaginary,n):
     k = 1
     result = 0
-    while(k <= n):
-        print("")
-        result +=  
+    while(k <= n-1 ):
+        result += combination(n, k) * (real ** k) * (imaginary ** (n - k)) * iota(n - k)
+        k += 2
+
+    return result
+
+def odd_real(real,imaginary,n):
+    k = 1
+    result = 0
+    while(k <= n ):
+        result += combination(n, k) * (real ** k) * (imaginary ** (n - k)) * iota(n - k)
+        k += 2
+
+    return result
+
+def odd_imaginary(real,imaginary,n):
+    k = 0
+    result = 0
+    while(k <= n-1 ):
+        result += combination(n, k) * (real ** k) * (imaginary ** (n - k)) * iota(n - k)
+        k += 2
+
+    return result
+
+
+# def till_n_minus_one(real,imaginary,n):
+#     k = 0
+#     result = 0
+#     while(k <= n-1):
+#         result += combination(n,k)*(real**k)*(imaginary**(n-k))*iota(n-k)
+#         k+=2
+#
+#     return result
+
+
+def algorithm(real,imaginary,n,plots):
+    if n%2 == 0:
+        R = even_real(real,imaginary,n)
+        I = even_imaginary(real, imaginary, n)
+        print(R,I)
+    else:
+        R = odd_real(real, imaginary, n)
+        I = odd_imaginary(real,imaginary,n)
+        print(R,I)
 
