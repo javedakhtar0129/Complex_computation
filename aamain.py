00b5d21d6e370ad167273137134078b740e3f4c5
JAVED AKHTAR <javedakhtar@JAVEDs-MacBook-Air.local>
2023-03-08 04:23:13 +0530

    added dunder methods

--- aamain.py
+++ aamain.py
@@ -1,76 +1,74 @@
-from tkinter import *
-from abAlgorithm import *
-
-
-window = Tk()
-window.title("Computational Software")
-# window.configure(background="white")
-window.geometry('500x185')
-
-def submit():
-    R = int(real.get())
-    I = int(imaginary.get())
-    N = int(power_text.get())
-    Plot = int(plot_text.get())
-    XX = []
-    YY = []
-    algorithm(R,I,N,Plot,XX,YY)
-
-
-
-def real_click(event):
-    real.delete(0,END)
-def imagine_click(event):
-    imaginary.delete(0,END)
-
-#creating a label
-label1 = Label(window,text="Plotting Complex Numbers", font=('Oswald 40'),fg="#b30000",height=1)
-label1.place(x=15, y=0)
-
-label2 = Label(window,text="(            +  i           )", font=('Oswald 40'),fg="#2eb82e",height=1)
-label2.place(x=15, y=65)
-
-label3 = Label(window,text="n", font=('Oswald 25'),fg="#2eb82e",height=1)
-label3.place(x=290, y=65)
-
-real = Entry(window,bg="#bbff99",fg="navy blue",borderwidth=3)
-real.insert(0,"REAL")
-real.bind("<Button-1>", real_click)
-real.place(x=35, y=75,height=40,width=90)
-
-imaginary = Entry(window,bg="#bbff99",fg="navy blue",borderwidth=3)
-imaginary.insert(0,"IMAGINARY")
-imaginary.bind("<Button-1>", imagine_click)
-imaginary.place(x=190, y=75, height=40,width=90)
-
-# Drop down menu
-# power = StringVar()
-# power.set("POWER")
-# drop  = OptionMenu(window,power,"SQUARE","CUBE")
-# drop.place(x=15,y=150,width=150,height=40)
-
-label4 = Label(window,text="value of n: ",font=('Script 25'),fg="#009999")
-label4.place(x=15,y=135)
-
-# spinbox for power of the equation
-power_text = StringVar(window)
-power_text.set("Iterations")
-power = Spinbox(window,textvariable=power_text,from_=1,to=20,bg="#BFEFFF",fg="navy blue")
-power.place(x=130,y=138,width=50,height=30)
-
-label5 = Label(window,text="Number of plots: ",font=('Script 25'),fg="#009999")
-label5.place(x=200,y=135)
-
-# spinbox for number of plots
-plot_text = StringVar(window)
-plot_text.set("Iterations")
-plots = Spinbox(window,textvariable=plot_text,from_=1,to=10,bg="#BFEFFF",fg="navy blue")
-plots.place(x=385,y=138,width=50,height=30)
-
-# submit button
-submit = Button(window, text="SUBMIT",bg='red',fg="navy blue",activebackground="sky blue",padx=30, pady=30,command=submit)
-submit.configure(font=("Bold"))
-submit.place(x=330,y=75,height=45)
-
-
-window.mainloop()
+# from tkinter import *
+# from abAlgorithm import *
+# import pandas as pd
+#
+# window = Tk()
+# window.title("Computational Software")
+# # window.configure(background="white")
+# window.geometry('500x185')
+#
+# def submit():
+#     R = float(real.get())
+#     I = float(imaginary.get())
+#     N = int(power_text.get())
+#     Plot = int(plot_text.get())
+#
+#     algorithm(R,I,N,Plot)
+#
+#
+# def real_click(event):
+#     real.delete(0,END)
+# def imagine_click(event):
+#     imaginary.delete(0,END)
+#
+# #creating a label
+# label1 = Label(window,text="Plotting Complex Numbers", font=('Oswald 40'),fg="#b30000",height=1)
+# label1.place(x=15, y=0)
+#
+# label2 = Label(window,text="(            +  i           )", font=('Oswald 40'),fg="#2eb82e",height=1)
+# label2.place(x=15, y=65)
+#
+# label3 = Label(window,text="n", font=('Oswald 25'),fg="#2eb82e",height=1)
+# label3.place(x=290, y=65)
+#
+# real = Entry(window,bg="#bbff99",fg="navy blue",borderwidth=3)
+# real.insert(0,"REAL")
+# real.bind("<Button-1>", real_click)
+# real.place(x=35, y=75,height=40,width=90)
+#
+# imaginary = Entry(window,bg="#bbff99",fg="navy blue",borderwidth=3)
+# imaginary.insert(0,"IMAGINARY")
+# imaginary.bind("<Button-1>", imagine_click)
+# imaginary.place(x=190, y=75, height=40,width=90)
+#
+# # Drop down menu
+# # power = StringVar()
+# # power.set("POWER")
+# # drop  = OptionMenu(window,power,"SQUARE","CUBE")
+# # drop.place(x=15,y=150,width=150,height=40)
+#
+# label4 = Label(window,text="value of n: ",font=('Script 25'),fg="#009999")
+# label4.place(x=15,y=135)
+#
+# # spinbox for power of the equation
+# power_text = StringVar(window)
+# power_text.set("Iterations")
+# power = Spinbox(window,textvariable=power_text,from_=1,to=20,bg="#BFEFFF",fg="navy blue")
+# power.place(x=130,y=138,width=50,height=30)
+#
+# label5 = Label(window,text="Number of plots: ",font=('Script 25'),fg="#009999")
+# label5.place(x=200,y=135)
+#
+# # spinbox for number of plots
+# plot_text = StringVar(window)
+# plot_text.set("Iterations")
+# plots = Spinbox(window,textvariable=plot_text,from_=1,to=10,bg="#BFEFFF",fg="navy blue")
+# plots.place(x=385,y=138,width=50,height=30)
+#
+# # submit button
+# submit = Button(window, text="SUBMIT",bg='red',fg="navy blue",activebackground="sky blue",padx=30, pady=30,command=submit)
+# submit.configure(font=("Bold"))
+# submit.place(x=330,y=75,height=45)
+#
+#
+# window.mainloop()
--- abAlgorithm.py
+++ abAlgorithm.py
@@ -1,6 +1,45 @@
 import numpy as np
 import pandas as pd
-# using recursion for combination
+
+df = pd.DataFrame(columns=['real','imaginary','power','quadrant'])#,dtype=np.float64)
+
+
+def exponential_fitting(result):
+    fitted_base = float(np.format_float_scientific(result.base,precision=5).split("e")[0])
+    fitted_exp = float(np.format_float_scientific(result.base,precision=5).split("e")[1])
+    result.base = fitted_base
+    result.exp = result.exp+fitted_exp
+    return result
+
+
+class Exponential:
+    def __init__(self,base,exp):
+        self.base = base
+        self.exp = exp
+
+    def __mul__(self, other):
+        multiplied =  Exponential(round(self.base * other.base,5),self.exp+other.exp)
+        return exponential_fitting(multiplied)
+
+    def __rmul__(self,other):
+        r_multiplied = Exponential(round(self.base * other,5),self.exp)
+        return exponential_fitting(r_multiplied)
+
+    def __pow__(self, power, modulo=None):
+        powered = Exponential(self.base**power,self.exp*power)
+        return exponential_fitting(powered)
+
+    def __add__(self, other):
+        sum = 0
+        if self.exp != other.exp:
+            if self.exp > other.exp:
+                other.base = other.base * 10 ** (self.exp - other.exp)
+            else:
+                self.base = self.base * 10 ** (other.exp - self.exp)
+            sum = Exponential(self.base + other.base, abs(self.exp - other.exp))
+        else:
+            sum = Exponential(self.base + other.base, self.exp)
+        return exponential_fitting(sum)
 
 def combination(n,r):
     if r==0:
@@ -16,33 +55,50 @@ def iota(i):
 
 
 def leibniz_expansion(real, imaginary, n, k):
-    result = 0
+    result = Exponential(0,0)
     while(k <= n):
-        result += combination(n,k)*(real**k)*(imaginary**(n-k))*iota(n-k)
+        result += iota(n-k)*((combination(n,k)*(real**k))*(imaginary**(n-k)))
         k+=2
 
     return result
 
-def algorithm(real,imaginary,n,plots,XX,YY):
 
-    # XX.append(str(real))
-    # YY.append(str(imaginary))
+def algorithm(real,imaginary,n,plots):
 
-    if plots == 0:
-    #     print("REAL\n",XX)
-    #     print("IMAGINARY\n",YY)
-    #
-        return
+    real = pd.Series([np.format_float_scientific(real,precision=5)])#,dtype=np.float64)
+    imaginary = pd.Series([np.format_float_scientific(imaginary,precision=5)])#,dtype=np.float64)
 
-    print("(", plots, ")")
-    print("Real: ", real, "\nImaginary: ", imaginary)
-    print()
+    R = Exponential(float(real[0].split('e')[0]),float(real[0].split('e')[1]))
+    I = Exponential(float(imaginary[0].split('e')[0]), float(imaginary[0].split('e')[1]))
 
-    if n%2 == 0:
-        R = leibniz_expansion(real, imaginary, n, 0)
-        I = leibniz_expansion(real, imaginary, n, 1)
-    else:
-        R = leibniz_expansion(real, imaginary, n, 1)
-        I = leibniz_expansion(real, imaginary, n, 0)
+    print(R.base,R.exp)
+    print(I.base,I.exp)
+    L = I+R
+    # L = (-1)*((2*(R**3))*(I**4))
+    print(L.base,L.exp)
 
-    algorithm(R,I,n,plots-1,XX,YY)
+    # for i in range(plots):
+    #     df.iloc[i] = [real,imaginary,n,i]
+    #     if n % 2 == 0:
+    #         R = leibniz_expansion(R, I, n, 0)
+    #         I = leibniz_expansion(R, I, n, 1)
+    #     else:
+    #         R = leibniz_expansion(R, I, n, 1)
+    #         I = leibniz_expansion(R, I, n, 0)
+
+
+    # for i in range(plots):
+    # #     print(f"R:{real},I:{imaginary},iteration:{i}")
+    #     if n%2 == 0:
+    #         r_m = leibniz_expansion(real[0], imaginary[0], n, 0)
+    #         i_m = leibniz_expansion(real[0], imaginary[0], n, 1)
+    #     else:
+    #         r_m = leibniz_expansion(real[0], imaginary[0], n, 1)
+    #         i_m = leibniz_expansion(real[0], imaginary[0], n, 0)
+
+    print(df)
+
+
+# algorithm(float(0.00000134234),float(.0091231),2,5)
+# algorithm(float(.0091231),float(.0091231),2,5)
+algorithm(float(.00023422),float(512.1241),2,5)
